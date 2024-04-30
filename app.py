3568c7971d427c01d2dee0b12656aa584bdc46f8
Colin Bertin <colinbertin12@gmail.com>
2024-04-30 16:49:38 +0900

    add login required

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
+from functools import wraps
 import pymongo
 import json
-
-app = Flask(__name__)
-app.secret_key = b'\x7f\xf5\xc0\xf3\xd4\x03\xa8\x03\xc5\x81\x85\x14\x12\xd2Z!'
 
 # Load config from config.json
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
+
+
+app = Flask(__name__)
+app.secret_key = config['SESSION_KEY']
+
 
 # Access API key
 mongo_uri = config['MONGO_URI']
@@ -15,6 +18,17 @@ mongo_uri = config['MONGO_URI']
 # Database
 client = pymongo.MongoClient(mongo_uri)
 db = client.task_management
+
+# Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+
+    return wrap
 
 #  Routes
 from user import routes
@@ -24,5 +38,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/dashboard/')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
